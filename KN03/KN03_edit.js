24d8b7a26b29b861dee56e81341d180ae5ddee9a
elmar-kessler <113603505+ElmarKessler@users.noreply.github.com>
2024-07-08 15:01:57 +0200

    Update KN03_edit.js

--- KN03/KN03_edit.js
+++ KN03/KN03_edit.js
@@ -16,14 +16,15 @@ db.books.updateMany(
     { $set: { pages: 250 } }
 );
 
-var meetingToReplace = ObjectId("60c72b2f9af1f9b3b4dcef60");
-db.meeting.replaceOne(
-    { _id: meetingToReplace },
+var memberToReplace = ObjectId("60c72b2f9af1f9b3b4dcef57");
+
+db.member.replaceOne(
+    { _id: memberToReplace },
     {
-        _id: meetingToReplace,
-        date: new Date("2023-06-15"),
-        bookToDiscuss: "Book Two Updated",
-        duration: 3.0,
-        attendees: 20
+        _id: memberToReplace,
+        name: "Updated Name",
+        gender: "non-binary",
+        age: 29
     }
 );
+
