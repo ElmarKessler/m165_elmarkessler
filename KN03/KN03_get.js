389b8d4ebee0091828ed2c85b34ac2b8353e0d6c
elmar-kessler <113603505+ElmarKessler@users.noreply.github.com>
2024-07-08 14:46:20 +0200

    Update KN03_get.js

--- KN03/KN03_get.js
+++ KN03/KN03_get.js
@@ -7,9 +7,6 @@ print("Abfrage 2:");
 db.books.find({ author: { $regex: ".*Author.*" } }).forEach(printjson);
 
 print("Abfrage 3:");
-db.meeting.find({ date: { $gte: new Date("2023-01-01") } }).forEach(printjson);
-
-print("Abfrage 4:");
 db.readinglist.find({
     $or: [
         { bookcount: { $gt: 10 } },
@@ -17,11 +14,11 @@ db.readinglist.find({
     ]
 }).forEach(printjson);
 
-print("Abfrage 5:");
+print("Abfrage 4:");
 db.books.find({
     pages: { $gt: 250 },
     author: "Author 2"
 }).forEach(printjson);
 
-print("Abfrage 6:");
+print("Abfrage 5:");
 db.members.find({}, { _id: 0, name: 1, gender: 1, age: 1 }).forEach(printjson);
